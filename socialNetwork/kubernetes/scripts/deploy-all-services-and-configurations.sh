0fca7c0b51f073c4876415a81d66bcc38599e74b
stvdputten <stvdputtenjur@gmail.com>
2021-11-07 17:51:00 +0100

    Update k8s

--- socialNetwork/kubernetes/scripts/deploy-all-services-and-configurations.sh
+++ socialNetwork/kubernetes/scripts/deploy-all-services-and-configurations.sh
@@ -4,8 +4,6 @@ cd $(dirname $0)/..
 NS="social-network"
 
 kubectl create namespace ${NS}
-# kubectl adm policy add-scc-to-user anyuid -z default -n ${NS}
-# kubectl adm policy add-scc-to-user privileged -z default -n ${NS}
 
 ./scripts/create-all-configmap.sh 
 
@@ -19,7 +17,7 @@ done
 # kubectl expose service nginx-thrift -n ${NS}
 # kubectl expose service jaeger-out -n ${NS}
 
-echo "After all pods are running (oc get pods):"
+echo "After all pods are running (kubectl get pods):"
 echo "Follow the instructions in openshift/README.md to configure and run init_social_graph.py to load the dataset."
 
 cd - >/dev/null
