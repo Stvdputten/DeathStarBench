fbe1ff1017d77ae5554f38a353d66a3c6d7396fb
stvdputten <stvdputtenjur@gmail.com>
2021-12-02 14:57:25 +0100

    Update workload hr and mm

--- hotelReservation/wrk2/workload.sh
+++ hotelReservation/wrk2/workload.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-./wrk -D exp -t 2 -c 4 -d 30s -L -s ./wrk2_lua_scripts/mixed-workload_type_1.lua http://localhost:5000 -R 200
+if [ -z "$nginx_ip" ]
+  then
+    echo "No argument supplied"
+    echo "using default"
+    ./wrk -D exp -t 2 -c 4 -d 30s -L -s ./wrk2_lua_scripts/mixed-workload_type_1.lua http://localhost:5000 -R 200
+  else
+    ./wrk -D exp -t 2 -c 4 -d 30s -L -s ./wrk2_lua_scripts/mixed-workload_type_1.lua http://$nginx_ip:5000 -R 200
+fi
--- mediaMicroservices/wrk2/workload.sh
+++ mediaMicroservices/wrk2/workload.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-./wrk -D exp -t 2 -c 4 -d 30s -L -s ./scripts/media-microservices/compose-review.lua http://localhost:8080/wrk2-api/review/compose -R 200
+if [ -z "$nginx_ip" ]
+  then
+    echo "No argument supplied"
+    echo "using default"
+    ./wrk -D exp -t 2 -c 4 -d 30s -L -s ./scripts/media-microservices/compose-review.lua http://localhost:8080/wrk2-api/review/compose -R 200
+  else
+    ./wrk -D exp -t 2 -c 4 -d 30s -L -s ./scripts/media-microservices/compose-review.lua http://$nginx_ip:8080/wrk2-api/review/compose -R 200
+fi
