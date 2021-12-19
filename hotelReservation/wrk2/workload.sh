8e2c3571b24d9c83c2cdeaa51207f2da633fbfc6
stvdputten <stvdputtenjur@gmail.com>
2021-12-19 13:05:06 +0100

    Fix bug #4
    Old bug else

--- hotelReservation/wrk2/workload.sh
+++ hotelReservation/wrk2/workload.sh
@@ -15,7 +15,7 @@ then
       echo "No argument supplied nginx"
       echo "using default args"
       ./wrk -D exp -t 2 -c 4 -d 30s -L -s ./wrk2_lua_scripts/mixed-workload_type_1.lua http://localhost:5000 -R 200
-    else 
+  else 
       echo "No argument supplied nginx"
       echo "using  args: threads=$threads and connections=$connections and duration=$duration and requests=$requests"
       ./wrk -D exp -t $threads -c $connections -d "$duration"s -L -s ./wrk2_lua_scripts/mixed-workload_type_1.lua http://localhost:5000 -R requests
@@ -26,8 +26,7 @@ else
       echo "Using argument nginx"
       echo "using default args"
       ./wrk -D exp -t 2 -c 4 -d 30s -L -s ./wrk2_lua_scripts/mixed-workload_type_1.lua http://$nginx_ip:5000 -R 200
-    else 
-    else
+  else 
       echo "Using argument nginx"
       echo "using  args: threads=$threads and connections=$connections and duration=$duration and requests=$requests"
       ./wrk -D exp -t $threads -c $connections -d "$duration"s -L -s ./wrk2_lua_scripts/mixed-workload_type_1.lua http://$nginx_ip:5000 -R $requests
--- mediaMicroservices/wrk2/workload.sh
+++ mediaMicroservices/wrk2/workload.sh
@@ -15,7 +15,7 @@ then
       echo "No argument supplied nginx"
       echo "using default args"
       ./wrk -D exp -t 2 -c 4 -d 30s -L -s ./scripts/media-microservices/compose-review.lua http://localhost:8080/wrk2-api/review/compose -R 200
-    else 
+  else 
       echo "No argument supplied nginx"
       echo "using  args: threads=$threads and connections=$connections and duration=$duration and requests=$requests"
       ./wrk -D exp -t $threads -c $connections -d "$duration"s -L -s ./scripts/media-microservices/compose-review.lua http://localhost:8080/wrk2-api/user-timeline/read -R $requests
@@ -26,8 +26,7 @@ else
       echo "Using argument nginx"
       echo "using default args"
       ./wrk -D exp -t 2 -c 4 -d 30s -L -s ./scripts/media-microservices/compose-review.lua http://$nginx_ip:8080/wrk2-api/review/compose -R 200
-    else 
-    else
+  else
       echo "Using argument nginx"
       echo "using  args: threads=$threads and connections=$connections and duration=$duration and requests=$requests"
       ./wrk -D exp -t $threads -c $connections -d "$duration"s -L -s ./scripts/media-microservices/compose-review.lua http://$nginx_ip:8080/wrk2-api/user-timeline/read -R $requests
