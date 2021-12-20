99fd992f6d0edb6ea6b9eea235d7dd402b2253fc
stvdputten <stvdputtenjur@gmail.com>
2021-12-20 12:05:44 +0100

    Added mixed workload to social-network

---
+++ socialNetwork/wrk2/workload-mixed.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+while getopts t:c:d:R: flag
+do
+    case "${flag}" in
+        t) threads=${OPTARG};;
+        c) connections=${OPTARG};;
+        d) duration=${OPTARG};;
+        R) requests=${OPTARG};;
+    esac
+done
+if [ -z "$nginx_ip" ]
+then
+  if [ -z "$threads" ] || [ -z "$connections" ]  || [ -z "$duration" ] || [ -z "$requests" ]
+    then
+      echo "No argument supplied nginx"
+      echo "using default args"
+      ./wrk -D exp -t 2 -c 4 -d 30s -L -s ./scripts/social-network/mixed-workload.lua http://localhost:8080 -R 200
+  else 
+      echo "No argument supplied nginx"
+      echo "using  args: threads=$threads and connections=$connections and duration=$duration and requests=$requests"
+      ./wrk -D exp -t $threads -c $connections -d "$duration"s -L -s ./scripts/social-network/mixed-workload.lua http://localhost:8080 -R $requests
+  fi
+else
+  if [ -z "$threads" ] || [ -z "$connections" ] || [ -z "$duration" ] || [ -z "$requests" ]
+    then
+      echo "Using argument nginx"
+      echo "using default args"
+      ./wrk -D exp -t 2 -c 4 -d 30s -L -s ./scripts/social-network/mixed-workload.lua http://$nginx_ip:8080 -R 200
+  else 
+      echo "Using argument nginx"
+      echo "using  args: threads=$threads and connections=$connections and duration=$duration and requests=$requests"
+      ./wrk -D exp -t $threads -c $connections -d "$duration"s -L -s ./scripts/social-network/mixed-workload.lua http://$nginx_ip:8080 -R $requests
+  fi
+fi
