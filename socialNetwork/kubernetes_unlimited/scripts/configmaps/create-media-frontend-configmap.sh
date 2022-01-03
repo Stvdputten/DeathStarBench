3f066d0d2f978c60200a344b623de7d193d6dd93
stvdputten <stvdputtenjur@gmail.com>
2022-01-03 18:44:35 +0100

    Add sn lim/unlim

--- socialNetwork/kubernetes_unlimited/scripts/configmaps/create-media-frontend-configmap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd $(dirname $0)/../..
-
-kubectl create cm media-frontend-nginx --from-file=media-frontend-config/nginx.conf  -n social-network
-kubectl create cm media-frontend-lua   --from-file=media-frontend-config/lua-scripts -n social-network
